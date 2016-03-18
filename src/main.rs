fb25c5374c90b5aee1176465f5be2641969fb1e5
Bryan Mytko <bryanmytko@gmail.com>
2016-03-18 00:48:23 -0400

    Add rtm.start connection and WS connection

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,58 @@
 #![plugin(dotenv_macros)]
 extern crate dotenv;
 extern crate hyper;
+extern crate websocket;
+extern crate rustc_serialize as serialize;
 
 use std::io::Read;
 
 use hyper::Client;
-use hyper::header::Connection;
 use hyper::header::{Headers, ContentType};
-use hyper::mime::{Mime, TopLevel, SubLevel};
+
+use websocket::client::request::Url;
+use websocket::{Client as WSClient, Message, Sender, Receiver};
+use websocket::message::Type;
+
+use serialize::json::Json;
 
 fn main() {
-    println!("{}", &dotenv!("APIKEY"));
-
+    /* @TODO Move to handshake / connect module */
     let client = Client::new();
     let mut headers = Headers::new();
     headers.set(ContentType::form_url_encoded());
 
-    /* Example Request
-       https://slack.com/api/chat.postMessage?
-       token=xxxxxx&channel=%23carls-place&text=hi&pretty=1 */
-
     let request_string = concat!(
         "token=",
-        dotenv!("APIKEY"),
-        "&username=carl_winslow&channel=%23carls-place&text=hi&pretty=1"
+        dotenv!("APIKEY")
     );
 
-    let mut res =
+    let mut handshake =
         client.post("https://slack.com/api/rtm.start")
         .body(request_string)
         .headers(headers)
         .send()
         .unwrap();
 
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
+    let mut response = String::new();
+    handshake.read_to_string(&mut response).unwrap();
 
-    println!("Response: {}", body);
+    let json_response = Json::from_str(&response).unwrap();
+    let json_response_object = json_response.as_object().unwrap();
+    let ws_url = json_response_object.get("url").unwrap();
+    let ws_url_string = ws_url.as_string().unwrap();
+
+    let uri = Url::parse(ws_url_string).unwrap();
+
+    println!("{}", uri);
+
+    let request = WSClient::connect(uri).unwrap();
+    let response = request.send().unwrap();
+
+    match response.validate() {
+        Ok(()) => { println!("Connected...") },
+        Err(e) => { println!("{:?}", e); }
+    }
+
+    /* @TODO Spawn threads for send / receive loops */
+
 }
