baca64fda61d9b6626239f2a3d4bebd749c87b24
Bryan Mytko <bryanmytko@gmail.com>
2016-03-19 03:10:45 -0400

    Add main loop with command prompt

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ use websocket::message::Type;
 
 use serialize::json::Json;
 
+use std::io::stdin;
+use std::io::{self, Write};
+
 mod connection;
 use connection::Connection;
 
@@ -26,10 +29,48 @@ fn main() {
     let response = request.send().unwrap();
 
     match response.validate() {
-        Ok(()) => { println!("Connected...") },
-        Err(e) => { println!("{:?}", e); }
+        Ok(()) => {
+            println!("\nConnected! Welcome to Carl Winslow Bot. Enter a command:");
+            println!("(type \\q to quit)\n");
+            print_prompt();
+        },
+        Err(e) => { println!("Error {:?}", e); }
     }
 
-    /* @TODO Spawn threads for send / receive loops */
+    /* Start main loop */
+    loop {
+        let mut command = String::new();
+        stdin().read_line(&mut command).unwrap();
+        let formatted_command = command.trim();
 
+
+        let message = match formatted_command {
+            "\\q" => {
+                println!("Disconnecting!");
+                break;
+            },
+            _ => {
+                io::stdout().write(b"Unknown Command!\n");
+                print_prompt();
+            }
+            // "ping" => {
+            //     Message::ping(b"PING".to_vec()),
+            //     _ => Message::text(trimmed.to_string()),
+            //  }
+        };
+
+        // match tx.send(message) {
+        //     Ok(()) => (),
+        //     Err(e) => {
+        //         println!("Main Loop: {:?}", e);
+        //         break;
+        //     }
+        // }
+    }
+
+    fn print_prompt(){
+        io::stdout().flush();
+        io::stdout().write(b"> ");
+        io::stdout().flush();
+    }
 }
