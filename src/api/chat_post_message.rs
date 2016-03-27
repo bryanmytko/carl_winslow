dde1481902e8f033205b767d3a2a3ee53cd6ba23
Bryan Mytko <bryanmytko@gmail.com>
2016-03-26 20:06:32 -0400

    Rename chatPostMessage to snakecase (Rust style)

---
+++ src/api/chat_post_message.rs
@@ -0,0 +1,25 @@
+pub fn send(message: &str) -> Result<::hyper::client::Response, ::hyper::error::Error> {
+    const METHOD: &'static str = "chat.postMessage";
+
+    let mut headers = ::api::set_headers();
+    let client = ::api::set_client(&mut headers);
+
+    // @TODO This stuff is just for testing. Pull actual data off the bot connection.
+    let mut request_string = String::new();
+    request_string.push_str("token=");
+    request_string.push_str(dotenv!("APIKEY"));
+    request_string.push_str("&channel=D0TABF474");
+    request_string.push_str("&text=");
+    request_string.push_str(message);
+    request_string.push_str("&username=carl_winslow");
+    request_string.push_str("&icon_url=");
+    request_string.push_str(::api::BOT_IMG);
+
+    let mut request_uri = String::from(::api::API_URI);
+    request_uri.push_str(METHOD);
+
+    client.post(request_uri.as_str())
+        .body(&request_string)
+        .headers(headers)
+        .send()
+}
