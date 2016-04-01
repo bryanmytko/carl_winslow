9fd943091022de0ccacb3002f5ba74616f828659
Bryan Mytko <bryanmytko@gmail.com>
2016-04-01 18:01:04 -0400

    Add return Option<String> to custom handler

--- src/handler/custom_handler.rs
+++ src/handler/custom_handler.rs
@@ -1,12 +1,12 @@
 use api::*;
 use websocket::{Message};
 
-pub fn send(message: &Message, command: &str) -> String {
+pub fn send(message: &Message, command: &str) -> Option<String> {
     let date = regex!(r"^\d{4}-\d{2}-\d{2}$");
 
     match command {
         "hi" => chat_post_message::send(message, "Hi, it's Carl!"),
         _ if date.is_match(command) => chat_post_message::send(message, "That's a date!"),
-        _ => chat_post_message::send(message, "I didn't understand that command."),
+        _ => None,
     }
 }
