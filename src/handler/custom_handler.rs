d7483856ab6e600561c23399f1056b8d17d7a489
Bryan Mytko <bryanmytko@gmail.com>
2016-04-02 16:38:01 -0400

    Rename rtm::chat_post_message to rtm::message

--- src/handler/custom_handler.rs
+++ src/handler/custom_handler.rs
@@ -5,8 +5,8 @@ pub fn send(message: &Message, command: &str) -> Option<String> {
     let date = regex!(r"^\d{4}-\d{2}-\d{2}$");
 
     match command {
-        "hi" => chat_post_message::send(message, "Hi, it's Carl!"),
-        _ if date.is_match(command) => chat_post_message::send(message, "That's a date!"),
+        "hi" => message::send(message, "Hi, it's Carl!"),
+        _ if date.is_match(command) => message::send(message, "That's a date!"),
         _ => None,
     }
 }
--- src/rtm/mod.rs
+++ src/rtm/mod.rs
@@ -1,4 +1,1 @@
-pub use hyper::Client;
-pub use hyper::header::{Headers, ContentType};
-
-pub mod chat_post_message;
+pub mod message;
