d9e1251005461b7c5109ff033cf7922ad1a82fd9
Bryan Mytko <bryanmytko@gmail.com>
2016-04-25 16:18:50 -0400

    Add exit ticket command

--- src/handler/custom_handler.rs
+++ src/handler/custom_handler.rs
@@ -4,10 +4,12 @@ use handler::{CustomConfig};
 
 pub fn send(message: &Message, command: &str, config: &CustomConfig) -> Option<String> {
     let date = regex!(r"^\d{4}-\d{2}-\d{2}$");
+    let exit = regex!(r"^\s*exit[t\s]*ticket[s]?");
 
     match command {
         "hi" => message::send(message, &config.greeting),
         "type" => typing::send(message),
+        _ if exit.is_match(command) => message::send(message, &config.exit_ticket),
         _ if date.is_match(command) => message::send(message, "That's a date!"),
         _ => None,
     }
